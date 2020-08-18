b3d15d8c0a78f4929d7b87074188008141c8358f
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-08-18 20:55:12 +0900

    edit emacs settings

---
+++ .emacs.d/custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+	 ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
+ '(custom-safe-themes
+	 (quote
+		("c7eb06356fd16a1f552cfc40d900fe7326ae17ae7578f0ef5ba1edd4fdd09e58" default)))
+ '(package-selected-packages
+	 (quote
+		(elscreen undo-tree undohist wgrep auto-complete ample-zen-theme multi-term))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,8 @@
+;;; INITIAL SETTINGS
+;; hide startup page
+(setq inhibit-startup-screen t)
+
+;; add load-path
 (defun add-to-load-path (&rest paths)
   (let (path)
     (dolist (path paths paths)
@@ -6,8 +11,6 @@
 	(add-to-list 'load-path default-directory)
 	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
 	    (normal-top-level-add-subdirs-to-load-path))))))
-
-;; add load-path
 (add-to-load-path "elisp" "conf" "public_repos")
 
 ;; split custom file to .emacs.d/conf/custom.el
@@ -16,7 +19,100 @@
   (write-region "" nil custom-file))
 (load custom-file)
 
-(setq auto-save-default nil)
-(setq make-backup-files nil)
 
 
+;;; ELPA REPOSITORY
+(require 'package)
+
+;; add MELPA
+(add-to-list
+ 'package-archives
+ '("melpa" . "https://melpa.org/packages/"))
+
+;; add marmalade
+(add-to-list
+ 'package-archives
+ '("marmalade" . "https://marmalade-repo.org/packages/"))
+
+(package-initialize)
+
+
+
+;;; AUTOMATE
+;; backup
+(add-to-list 'backup-directory-alist (cons "."  "~/.emacs.d/backups/"))
+
+;; auto save
+(setq auto-save-file-name-transforms
+			`((".*", "~/.emacs.d/backups/" t)))
+(setq auto-save-timeout 5)
+(setq auto-save-list-file-prefix nil)
+
+;; load when the file was changed
+(global-auto-revert-mode t)
+
+;; auto complete
+(when (require 'auto-complete-config nil t)
+	(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+	(ac-config-default)
+	(setq ac-use-menu-map t)
+	(setq ac-ignore-case nil))
+
+
+
+;;; EMACS STYLES
+;; theme
+(load-theme 'ample-zen t)
+
+;; keybinds
+(define-key global-map (kbd "C-t") 'other-window)
+(define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
+
+;; add column number
+(column-number-mode t)
+
+;; frame title format
+(setq-default frame-title-format "%f")
+
+;; paren
+(setq show-paren-delay 0)
+(setq show-paren-style 'parenthesis)
+(set-face-attribute 'show-paren-match nil
+		    :background 'unspecified)
+(set-face-background 'show-paren-match "blue")
+(show-paren-mode t)
+
+;; indent
+(setq-default tab-width 2)
+
+
+
+;;; EMACS LISP MODE HOOKS
+(defun elisp-mode-hooks ()
+  "lisp-mode-hooks"
+  (when (require 'eldoc nil t)
+    (setq eldoc-idle-delay 0.2)
+    (setq eldoc-echo-area-use-multiline-p t)
+    (turn-on-eldoc-mode)))
+(add-hook 'emacs-lisp-mode-hook 'elisp-mode-hooks)
+
+
+
+;;; OTHER PKGS
+;; edit something searched by grep
+(require 'wgrep nil t)
+
+;; undo history
+(when (require 'undohist nil t)
+	(undohist-initialize))
+
+;; undo tree
+(when (require 'undo-tree nil t)
+	(global-undo-tree-mode))
+
+;; elscreen
+(when (require 'elscreen nil t)
+	(elscreen-start)
+	(if window-system
+			(define-key elscreen-map (kbd "C-z") 'iconify-or-deiconify-frame)
+		(define-key elscreen-map (kbd "C-z") 'suspend-emacs)))
