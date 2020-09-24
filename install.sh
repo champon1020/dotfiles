3e5d7c00ef0026c519602c97397f4ebc688db725
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-09-24 13:58:00 +0900

    fix indent style of install.sh

--- install.sh
+++ install.sh
@@ -10,22 +10,22 @@ IGNORE_FILES[1]=$CUR/.gitignore
 # IGNORE_FILES=($CUR/. $CUR/.. $CUR/.git $CUR/.gitignore)
 
 for file in ${FILES[@]}; do
-		if [[ ! ${IGNORE_FILES[@]} =~ $file ]]; then
-			  TARGET=$HOME/$(basename $file)
-				if [ -e $TARGET ]; then
-						if [ -L $TARGET ]; then
-								unlink $TARGET
-								echo "unlink '$TARGET'"
-						else
-								if [ ! -e $HOME/.backups ]; then
-										mkdir $HOME/.backups
-										echo "create directory '$HOME/.backups/'"
-								fi
-								mv $TARGET $HOME/.backups/
-								echo "backup '$TARGET' to '$HOME/.backups/'" 
-						fi
+	if [[ ! ${IGNORE_FILES[@]} =~ $file ]]; then
+		TARGET=$HOME/$(basename $file)
+		if [ -e $TARGET ]; then
+			if [ -L $TARGET ]; then
+				unlink $TARGET
+				echo "unlink '$TARGET'"
+			else
+				if [ ! -e $HOME/.backups ]; then
+					mkdir $HOME/.backups
+					echo "create directory '$HOME/.backups/'"
 				fi
-				ln -s $file $TARGET
-				echo "link '$file' to '$TARGET'"
+				mv $TARGET $HOME/.backups/
+				echo "backup '$TARGET' to '$HOME/.backups/'" 
+			fi
 		fi
+		ln -s $file $TARGET
+		echo "link '$file' to '$TARGET'"
+	fi
 done
