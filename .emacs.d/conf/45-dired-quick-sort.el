55df9a210e27f9f442e27748444ecab6d80e41a0
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-10-08 01:59:05 +0900

    modification for mac (#1)
    * update for mac
    
    * update custom.el

--- .emacs.d/conf/45-dired-quick-sort.el
+++ .emacs.d/conf/45-dired-quick-sort.el
@@ -4,8 +4,9 @@
 ;; This package provides sorting directory first.
 
 ;;; Code:
-(when (require 'dired-quick-sort nil t)
-	(dired-quick-sort-setup))
+(when (if (not (eq system-type 'darwin)) t nil)
+	(when (require 'dired-quick-sort nil t)
+		(dired-quick-sort-setup)))
 
 (provide '45-dired-quick-sort)
 ;;; 45-dired-quick-sort.el ends here
