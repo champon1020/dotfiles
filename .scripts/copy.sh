69458fa7612dd479800571bca3e37b1f0f1cddb0
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-11-06 13:21:50 +0900

    add bash script that copies file to clipboard

---
+++ .scripts/copy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# != 1 ]; then
+	echo "ERROR: the number of arguments must be 1, but got $#"
+	exit 1
+fi
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  cat $1 | xsel --clipboard --input
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  pgfile < $1
+fi
+
+echo "copied $1 to clipboard"
